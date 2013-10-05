ffacac8702144d89827919aa56db485d259859db
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-04 22:15:49 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirbnbClone::Application.config.secret_token = '7420440055ee640a72c9ba392542fa0f56364722e60a9e3e7e6945a4357ce256512a5378e67f87755d747dda6aa02ff99fad92aa12c2358dc4c58ed2fc40468f'
