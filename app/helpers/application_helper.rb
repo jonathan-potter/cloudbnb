d183717eee6dd339748c726c74881104b8868121
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-06 21:10:57 -0400

    creates checkbox_group in application helper to dry up views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def checkbox_group(options)
+    # options hash must include :collection, :ul_class, and :html_name
+    @ul = options
+    render "shared/checkbox_group"
+  end
+
 end
