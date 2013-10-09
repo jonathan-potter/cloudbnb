7a890fb39ad8a6ad3953f49b84dc659b76c7f137
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-08 21:06:43 -0400

    adds calendar with dates colored by their booking status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -32,21 +32,59 @@ class Booking < ActiveRecord::Base
     booking.public_send(method)
   end
 
+  def self.dates_this_month
+    day = Time.now.to_date
+    first_day = firstDay = day - day.day + 1
+
+    end_day = first_day + 27
+    until (end_day + 1).month != first_day.month
+      end_day += 1
+    end
+
+    [first_day, end_day]
+  end
+
+  def self.booked_dates_this_month_for_space(space)
+    bookings = Booking.where("start_date BETWEEN ? AND ?", )
+
+    first_and_last = self.dates_this_month
+    first = first_and_last[0]
+    last  = first_and_last[1]
+    bookings = Booking.where("space_id = ?"   , space.id)
+                      .where("? <= end_date"  , first)
+                      .where("? >= start_date", last)
+                      .where("approval_status = ?"     , Booking.approval_statuses[:approved])
+
+    booked_dates = Set.new
+    days = (first..last).to_a
+    bookings.each do |booking|
+      days.each do |day|
+        booked_dates << day if day.between?(booking.start_date, booking.end_date)
+      end
+    end
+
+    booked_dates
+  end
+
+  def conflicts_with_date?(date)
+    date.between(self.start_date, self.end_date)
+  end
+
   def set_approval_status(status)
     self.update_attributes!(approval_status: status)
   end
 
   def overlapping_requests(status)
-    CatRentalRequest
-    .where("space_id = ?"   , self.space_id)
-    .where("? <= end_date"  , self.start_date)
-    .where("? >= start_date", self.end_date)
-    .where("id != ?"        , self.id)
-    .where("status = ?"     , Booking.approval_statuses(status))
+    Booking
+    .where("space_id = ?"        , self.space_id)
+    .where("? <= end_date"       , self.start_date)
+    .where("? >= start_date"     , self.end_date)
+    .where("id != ?"             , self.id)
+    .where("approval_status = ?" , Booking.approval_statuses[status])
   end
 
   def decline_conflicting_pending_requests!
-    overlapping_pending_requests.each { |request| request.decline }
+    overlapping_requests(:pending).each { |request| request.decline }
   end
 
   def cancel_by_user
