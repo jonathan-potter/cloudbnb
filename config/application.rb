c190b8f825c4d5e98ea4fe5072914bb5199f7a7a
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-10 17:53:46 -0400

    adds 'config.assets.initialize_on_precompile = false' to application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module AirbnbClone
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # heroku stuff
+    config.assets.initialize_on_precompile = false
   end
 end
