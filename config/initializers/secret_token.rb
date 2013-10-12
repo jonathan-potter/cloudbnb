8913b67daf2b8c8a8fab126da9fceda668a668fd
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-12 15:27:33 -0400

    adds 'newrelic_rmp' gem, contains new application secret key, and hides both away in ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AirbnbClone::Application.config.secret_token = '7420440055ee640a72c9ba392542fa0f56364722e60a9e3e7e6945a4357ce256512a5378e67f87755d747dda6aa02ff99fad92aa12c2358dc4c58ed2fc40468f'
+AirbnbClone::Application.config.secret_token = ENV["RAILS_SECRET_TOKEN"]
