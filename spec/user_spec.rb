63625258d2ac9e1fac3f0ee005ab6cd11db4d8b3
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-16 16:47:25 -0400

    adds user model tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,32 +2,32 @@ require 'spec_helper'
 
 describe User do
 
-  params = { user: { first_name: "Applesauce",
+  params = { valid_user: { first_name: "Applesauce",
                       last_name: "Mapplesauce",
                           email: "Applesauce.Mapplesauce@example.com",
                        password: "password",
           password_confirmation: "password" }}
 
   subject(:user) do
-    User.create(params[:user])
+    User.create(params[:valid_user])
 
     User.first
   end
 
-  it "should store given first name" do
-    expect(user.first_name).to eq(params[:user][:first_name])
+  describe "stored in the database" do
+    it "should have a first name" do
+      expect(user.first_name).to eq(params[:user][:first_name])
+    end
+    it "should have a last name" do
+      expect(user.last_name).to eq(params[:user][:last_name])
+    end
+    it "should have an email" do
+      expect(user.email).to eq(params[:user][:email])
+    end
+    it "should not have it's password in plain text" do
+      expect(user.password).to eq(nil)
+    end
   end
-  it "should store given last name" do
-    expect(user.last_name).to eq(params[:user][:last_name])
-  end
-  it "should store given email" do
-    expect(user.email).to eq(params[:user][:email])
-  end
-  it "should not store password in plain text" do
-    expect(user.password).to_not eq(nil)
-  end
-  it "should not store password" do
-    expect(user.password).to_not eq(nil)
-  end
+
 
 end
