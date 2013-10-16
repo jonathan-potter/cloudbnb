855293d5697591944cd045313cdb276e4b7da38c
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-16 10:40:32 -0400

    adds pagination to space index lists

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,12 +3,15 @@ class SpacesController < ApplicationController
   before_filter :require_current_user!, only: [:new, :create]
 
   def index
+
     if params[:space_filters]
-      @spaces = Space.find_with_filters(params[:space_filters])
-      @spaces = @spaces[0..13]
+      space_relation  = Space.find_with_filters(params[:space_filters])
     else
-      @spaces = Space.find(limit: 14)
+      space_relation = Space
     end
+
+    page = params[:page] ? params[:page] : 1
+    @spaces = space_relation.page(page).per(14)
 
     @json = @spaces.to_gmaps4rails
   end
