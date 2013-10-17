a6d04ae72677368ea1947859f9effcf53faa0846
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-16 22:11:29 -0400

    fires an ajax request whenever any filter condition is changed on the spaces index page

--- app/assets/javascripts/space_index_form.js
+++ app/assets/javascripts/space_index_form.js
@@ -1,33 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $("#space-index-filter-form").on("ajax:success", function(event, data){
-
-    console.log("space-index-filter-form submitted")
-    var html = data
-    $("#space-list-wrapper").html(html);
+  $("#space-index-filter-form").find(":input").each(function(){
+    $(this).change(function(){
+      $("#space-index-filter-form").submit();
+    });
   });
 
-
-  // $("#cat-list").on("ajax:success", ".cat-favorite-form", function(event, data){
-  //   var $form = $(this);
-  //   var $li = $form.closest("li");
-  //
-  //   // data is the `cat` json object
-  //   if(data.is_favorite){
-  //     $li.addClass("is-favorite");
-  //   } else {
-  //     $li.removeClass("is-favorite");
-  //   }
-  // });
-  //
-  //
-  // $("#cat-new-form").on("ajax:success", function(event, data){
-  //   var $form = $(this);
-  //
-  //   // data is the `_cat_list_item` partial
-  //   $("#cat-list").append(data);
-  //
-  //   $form[0].reset();
-  // });
+  $("#space-index-filter-form").on("ajax:success", function(event, data){
+    $("#space-list-wrapper").html(data);
+  });
 
 });
