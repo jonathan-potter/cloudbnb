029e310907a41a38e797634cd5024d4bdf10345a
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-10-17 15:59:22 -0400

    reduces requested image sizes accross the site and adds numbered map markers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,13 +2,16 @@ var initializeMap = function() {
 
   var spaces = JSON.parse($("#bootstrap").html());
   var maxLat, minLat, maxLon, minLon;
+  var bounds = new google.maps.LatLngBounds();
 
   spaces.forEach(function(space) {
 
     if (!maxLat || maxLat < space["latitude"])  { maxLat = space["latitude"]};
-    if (!minLat || minLat < space["latitude"])  { minLat = space["latitude"]};
+    if (!minLat || minLat > space["latitude"])  { minLat = space["latitude"]};
     if (!maxLon || maxLon < space["longitude"]) { maxLon = space["longitude"]};
-    if (!minLon || minLon < space["longitude"]) { minLon = space["longitude"]};
+    if (!minLon || minLon > space["longitude"]) { minLon = space["longitude"]};
+
+    bounds.extend(new google.maps.LatLng(space["latitude"], space["longitude"]))
 
   });
 
@@ -23,17 +26,15 @@ var initializeMap = function() {
 
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
-  var bounds = new google.maps.LatLngBounds();
+  map.fitBounds(bounds)
 
-
-  function createMarker(lat, long, title, addy, completed) {
+  function createMarker(lat, long, title, addy, number) {
 
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(lat, long),
       map: map,
-      title: title
-      // icon: redMarker
-      // icon: completed ? greenMarker : redMarker
+      title: title,
+      icon: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=" + number + "|FFaaaa|333333"
     });
 
     google.maps.event.addListener(marker, 'click', function() {
@@ -48,10 +49,9 @@ var initializeMap = function() {
     });
   };
 
+  var index = 1;
   spaces.forEach(function(space) {
-
-    createMarker(space["latitude"], space["longitude"],
-    space["title"], space["address"]);
-
+    createMarker(space["latitude"], space["longitude"], space["title"], space["address"], index);
+    index++;
   });
 };
